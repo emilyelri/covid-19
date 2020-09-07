4ee7db45dfe2356c36785ef65460fcc9986cfcc0
Emily_Richard <richard.emily.e@gmail.com>
2020-09-07 17:26:12 -0400

    file structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import './styles/styles.css';
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
 
 export default App;
