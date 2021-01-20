856f17678fa7cdb6cb702369b3f0bbab3a766276
emilyelri <richard.emily.e@gmail.com>
2021-01-20 17:14:03 -0500

    init axios call to covid api in api folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Cards, Chart, CountryPicker } from './components'
+import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api';
 import styles from './App.module.css';
 
 class App extends React.Component {
