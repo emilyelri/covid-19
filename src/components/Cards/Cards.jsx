59ea92c7df354f8f7e9bc007562e4a72907b56b1
emilyelri <richard.emily.e@gmail.com>
2021-02-04 09:03:53 -0500

    added material-ui/core

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 const Cards = (props) => {
   console.log(props)
+
+
+
   return (
-    <h1>Cards</h1>
+    <div className="container">
+      
+    </div>
   )
 }
 
